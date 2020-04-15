2514f206cb92ad14503aa4aa767cc97a0b1144fa
Simon <39345336+crydotsnake@users.noreply.github.com>
2020-04-15 11:42:32 +0200

    Delete Social Network titles.

--- index.php
+++ index.php
@@ -25,16 +25,16 @@
 							<ul class="icons">
 								<?php 
 							if ($site->twitter()){
-								echo '<li><a target="_blank" href="'.$site->twitter().'" class="icon brands fa-twitter">Twitter</a></li>';
+								echo '<li><a target="_blank" href="'.$site->twitter().'" class="icon brands fa-twitter"></a></li>';
 								}
 							if ($site->instagram()){
-								echo '<li><a target="_blank" href="'.$site->instagram().'" class="icon brands fa-instagram">Instagram</a></li>';
+								echo '<li><a target="_blank" href="'.$site->instagram().'" class="icon brands fa-instagram"></a></li>';
 								}
 							if ($site->facebook()){
-								echo '<li><a target="_blank" href="'.$site->facebook().'" class="icon brands fa-facebook-f">Facebook</a></li>';
+								echo '<li><a target="_blank" href="'.$site->facebook().'" class="icon brands fa-facebook-f"></a></li>';
 								}
 							if ($site->github()){
-								echo '<li><a target="_blank" href="'.$site->github().'" class="icon brands fa-github">GitHub</a></li>';
+								echo '<li><a target="_blank" href="'.$site->github().'" class="icon brands fa-github"></a></li>';
 								}
 							?>
 							</ul>
